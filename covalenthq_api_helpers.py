eef5f2f1f995b9506e297c7fec6682b75231c902
hmspinafore <>
2022-09-25 17:23:23 -0700

    Request metadata only for Reddit Collectible items

--- covalenthq_api_helpers.py
+++ covalenthq_api_helpers.py
@@ -10,11 +10,25 @@ from requests.models import HTTPError
 logger = logging.getLogger("reddit_portfolio_logger")
 
 # https://www.covalenthq.com/docs/api/#/0/Get%20token%20balances%20for%20address/USD/137
-CHQ_BALANCE_URL = "https://api.covalenthq.com/v1/137/address/{address}/balances_v2/?quote-currency=USD&format=JSON&nft=true&no-nft-fetch=false&key={api_key}"
+CHQ_BALANCE_URL = "https://api.covalenthq.com/v1/137/address/{address}/balances_v2/?quote-currency=USD&format=JSON&nft=true&no-nft-fetch=true&key={api_key}"
+CHQ_GET_EXTERNAL_DATA = "https://api.covalenthq.com/v1/137/tokens/{contract_address}/nft_metadata/{token_id}/?quote-currency=USD&format=JSON&key={api_key}"
 
 HEADERS = {
     "Content-Type": "application/json",
 }
+
+def fetch_external_metadata(contract_address: str, token_id: int) -> dict:
+    response = requests.get(CHQ_GET_EXTERNAL_DATA.format(
+        contract_address=contract_address, token_id=token_id, api_key=config.chq_api_key))
+
+    if response.status_code != 200:
+        logger.debug(
+                f"[Covalent HQ] Failed to fetch metadata for {contract_address} token {token_id}."
+                f"Received {response.status_code}: {response.reason}. {response.json()}"
+        )
+        response.raise_for_status()
+
+    return response.json()["data"]["items"]
 
 def fetch_token_balance_for_address(address: str) -> list:
     """Fetches data about token balances from Covalent HQ's GET endpoint for given address.
@@ -62,7 +76,8 @@ def compute_token_balance_from_json_responses(responses: list[dict]) -> dict:
             continue
 
         for nft_data in response['nft_data']:
-            nft_name = nft_data['external_data']['name']
+            metadata = fetch_external_metadata(response["contract_address"], int(nft_data["token_id"]))
+            nft_name = metadata[0]['nft_data'][0]['external_data']['name']
             avatar_name_raw, avatar_id_str = [s.strip() for s in nft_name.split("#")]
             avatar_name, avatar_id = avatar_name_raw.strip(), int(avatar_id_str)
         
